86f28ea4e0aa162e13d938d20d4baa03608e2bd4
wangbowww <weirdowww@qq.com>
2026-03-14 19:42:42 +0800

    add cpp and pytorch dev envs

---
+++ CppCodes/test/build/CMakeFiles/codeplay.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/root/Codes/test/bin/codeplay"
+  "/root/Codes/test/bin/codeplay.pdb"
+  "CMakeFiles/codeplay.dir/link.d"
+  "CMakeFiles/codeplay.dir/src/test.cpp.o"
+  "CMakeFiles/codeplay.dir/src/test.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/codeplay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
